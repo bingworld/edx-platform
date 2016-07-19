a274093df0521d3de7425fa91e150af1d17d191c
Albert St. Aubin <astaubin@edx.org>
2016-07-19 09:57:10 -0400

    Removed constant that was not needed (to be squashed) TNL-4742

--- common/djangoapps/dark_lang/urls.py
+++ common/djangoapps/dark_lang/urls.py
@@ -6,7 +6,6 @@ from django.conf.urls import patterns, url
 
 from dark_lang import views
 
-app_name = 'darklang'
 urlpatterns = patterns(
     '',
     url(r'^$', views.DarkLangView.as_view(), name='preview_lang'),
